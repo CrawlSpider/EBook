760aa86aac5624e768cf52e1e25c4b3b264545c2
wangbo <wangbo@bkclouds.com>
2020-02-14 21:31:46 +0800

    进一步提高 "百度下载提取码" 的抽取准确性

--- EBook/spiders/nmod.py
+++ EBook/spiders/nmod.py
@@ -5,6 +5,7 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.loader.processors import MapCompose, Join
 from scrapy.http import FormRequest
+import logging
 ###############################################################################
 # NMOD.NET
 # 优质电子书免费下载
@@ -46,31 +47,96 @@ class NmodSpider(CrawlSpider):
         @scrapes title author format label date isbn content size download comment_cnt heart_cnt share_cnt
         """
         item = {}
-        #item['title'] = response.xpath(u'//div[contains(@class, "alert")]/p/img/@title').extract()
-        title = response.xpath(u'//table[contains(@class, "dltable")]//td/text()').re(u'(?<=文件名称：).+')
+        #
+        #
+        # 提取书名（目前发现有三个地方）
+        title = response.xpath(u'//table[contains(@class, "dltable")]//td/text()').re(u'(?<=文件名称[:：]).+')     # 提取文件下载处的书名
         if len(title) == 0:
-            item['title'] = response.xpath(u'//h1[contains(@class, "title")]/a/text()').extract()
+            # 如果文件下载处的书名没有，则页面最上面开头处的书名
+            title = response.xpath(u'//h1[contains(@class, "title")]/a/text()').extract()
+        if len(title) == 0:
+            # 提取书籍简介处的书名
+            title = response.xpath(u'//div[contains(@class, "alert")]/p[contains(text(), "书名")]/text()').re(u'(?<=书名[:： ]).+')
+        if len(title) == 0:
+            # 提取书籍简介处的书名--书名含有链接
+            title = response.xpath(u'//div[contains(@class, "alert")]/p[contains(text(), "书名")]/a/text()').extract()
+        if len(title) == 0:
+            self.logger.info("Sorry, I can't find the ebook name")
         else:
-            item['title'] = title
-        author = response.xpath(u'//article[contains(@class, "article-content")]//p/text()').re(u'(?<=作者[:：]).+')
-        item['author'] = MapCompose(str.strip)(author)   # str 对象是 python3 的语法，python2 请更换为 unicode
+            item['title'] = MapCompose(str.strip)(title)    # str 对象是 python3 的语法，python2 请更换为 unicode
+        #
+        #
+        # 提取书籍作者
+        #author = response.xpath(u'//article[contains(@class, "article-content")]//p/text()').re(u'(?<=作者[:：]).+')
+        author = response.xpath(u'//div[contains(@class, "alert")]/p[contains(text(), "作者")]/a/text()').extract()    # 作者名完全包含在链接
+        if len(author) == 0:    # 作者名完全不在链接
+            author = response.xpath(u'//div[contains(@class, "alert")]/p[contains(text(), "作者")]/text()').re(u'(?<=作者[:： ]).+')
+        #if len(author) == 0:
+            #tmp_head = response.xpath(u'//div[contains(@class, "alert")]/p[contains(text(), "作者")]/text()').re(u'(?<=作者[:： ]).+')
+            #tmp_list = response.xpath(u'//div[contains(@class, "alert")]/p[contains(text(), "作者")]/text()').extract()
+            #tmp_midd = response.xpath(u'//div[contains(@class, "alert")]/p[contains(text(), "作者")]/a/text()').extract()
+            #tmp_head.append(tmp_midd[0])
+            #tmp_head.append(tmp_list[1])
+            #author.join(tmp_head)
+        if len(author) == 0:
+            self.logger.info("Sorry, I can't find the author of the ebook")
+        else:
+            item['author'] = MapCompose(str.strip)(author)   # str 对象是 python3 的语法，python2 请更换为 unicode
+        #
+        #
+        # 提取书籍格式
         format = response.xpath(u'//article[contains(@class, "article-content")]//p/text()').re(u'(?<=格式[:：]).+')
         item['format'] = MapCompose(str.strip)(format)   # str 对象是 python3 的语法，python2 请更换为 unicode
-        label = response.xpath(u'//div[@class="article-tags"]/a/text()').extract()
+        #
+        #
+        # 提取书籍分类标签
+        #label = response.xpath(u'//div[@class="article-tags"]/a/text()').extract()
+        # 标签名含有链接
+        label = response.xpath(u'//div[contains(@class, "alert")]/p[contains(text(), "标签")]/a/text()').extract()
         if len(label) == 0:
-            item['label'] = response.xpath(u'//article[contains(@class, "article-content")]//p/text()').re(u'(?<=标签[:：]).+')
+            label = response.xpath(u'//article[contains(@class, "article-content")]//p/text()').re(u'(?<=标签[:：]).+')
+        if len(label) == 0:      # 标签名不含有链接
+            label = response.xpath(u'//div[contains(@class, "alert")]/p[contains(text(), "标签")]/text()').re(u'(?<=标签[:： ]).+')
+        if len(author) == 0:
+            self.logger.info("Sorry, I can't find the label of the ebook")
         else:
             item['label'] = label
+        #
+        #
+        # ISBN
         isbn = response.xpath(u'//div[contains(@class, "alert")]/p/text()').re(u'(?<=ISBN[:：]).*[0-9]+')
         item['isbn'] = MapCompose(str.strip)(isbn)   # str 对象是 python3 的语法，python2 请更换为 unicode
+        #
+        #
+        #
         size = response.xpath(u'//table//td/text()').re(u'(?<=大小：)[.0-9MmKkGgBb]+')
         fun_k2m = lambda x : str(u'%.2f'%(float(re.sub(u'[KB]', u'', x)) / 1024))
         fun_g2m = lambda x : str(u'%.2f'%(float(re.sub(u'[GB]', u'', x)) * 1024))
         size_mb = [x if u'M' in x else x if u'K' not in x else fun_k2m(x)+u'MB' for x in size]
         size_mb = [x if u'M' in x else x if u'G' not in x else fun_g2m(x)+u'MB' for x in size_mb]
         item['sizeMB'] = [re.sub('[kKMmGgBb]+', '', i) for i in size_mb]
-        item['ecode'] = response.xpath(u'//span/strong/text()').re(u'[a-zA-Z0-9]+')
+        #
+        #
+        # 
+        ecode = response.xpath(u'//span/strong/text()').re(u'[a-zA-Z0-9]+')
+        if len(ecode) == 0:
+            ecode = response.xpath(u'//div[contains(@style, "hidden")]/span/text()').re(u'[a-zA-Z0-9]+')
+        if len(ecode) == 0:
+            ecode = response.xpath(u'//div[contains(@style, "hidden")]/text()').re(u'[a-zA-Z0-9]+')
+        if len(ecode) == 0:
+            ecode = response.xpath(u'//div[contains(@style, "hidden")]/strong/span/text()').re(u'[a-zA-Z0-9]+')
+        if len(ecode) == 0:
+            ecode = response.xpath(u'//div[contains(@style, "hidden")]/span/strong/span/text()').re(u'[a-zA-Z0-9]+')
+        if len(ecode) == 0:
+            self.logger.info("Sorry, I can't find the code of the ebook")
+        else:
+            item['ecode'] = ecode
+        #print(response.body.decode('utf-8'))
+        #
+        #
         item['book_page'] = response.url
+        #
+        #
         item['down_url'] = response.xpath(u'//table[@class="dltable"]//a/@href').re(u'(?<=url=).+')
 
         yield item
